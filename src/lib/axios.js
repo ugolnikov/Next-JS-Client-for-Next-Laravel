82c4484d81692533343ae7b563a4497bd5deec29
ugolnikov <hacker.2282005@mail.ru>
2024-12-19 13:58:49 +0500

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,6 +2,7 @@ import Axios from 'axios'
 
 const axios = Axios.create({
     baseURL: 'http://ugolnikov2.temp.swtest.ru/',
+    adapter: require('axios/lib/adapters/http'),
     // headers: {
     //     'X-Requested-With': 'XMLHttpRequest',
     // },
