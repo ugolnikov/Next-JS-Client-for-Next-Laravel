e117c77d2d6359462684e91028354bab18da3c56
ugolnikov <hacker.2282005@mail.ru>
2024-12-22 17:22:02 +0500

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    baseURL: 'https://next-backend.ct.ws/',
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
