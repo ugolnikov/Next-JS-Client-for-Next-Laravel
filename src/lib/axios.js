47ca222851ead94edc0524e56336d9cba9a546d7
ugolnikov <hacker.2282005@mail.ru>
2024-12-22 17:59:56 +0500

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,9 +2,9 @@ import Axios from 'axios'
 
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    // headers: {
-    //     'X-Requested-With': 'XMLHttpRequest',
-    // },
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+    },
     withCredentials: true,
     withXSRFToken: true
 })
