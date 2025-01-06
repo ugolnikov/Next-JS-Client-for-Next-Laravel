097b10bd10e6f2cbbf647b18cf463abda3ceebd6
ugolnikov <hacker.2282005@mail.ru>
2025-01-06 12:01:22 +0500

    upd axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,10 +4,8 @@ const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
-        'Access-Control-Allow-Origin': '*'
     },
-    withCredentials: true,
-    withXSRFToken: true
+    withCredentials: true, 
 })
 
 export default axios
