3f43fb1b71a6a9692b7edacd378a4f2d50e12bc5
ugolnikov <hacker.2282005@mail.ru>
2025-01-07 03:17:30 +0500

    +js-cookie +interceptor for x-csrf

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,4 +1,5 @@
 import Axios from 'axios'
+import Cookies from 'js-cookie' 
 
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
@@ -6,6 +7,17 @@ const axios = Axios.create({
         'X-Requested-With': 'XMLHttpRequest',
     },
     withCredentials: true, 
+})
+
+// Интерсептор для установки X-XSRF-TOKEN заголовка
+axios.interceptors.request.use(config => {
+    const token = Cookies.get('XSRF-TOKEN')
+    if (token) {
+        config.headers['X-XSRF-TOKEN'] = token
+    }
+    return config
+}, error => {
+    return Promise.reject(error)
 })
 
 export default axios
