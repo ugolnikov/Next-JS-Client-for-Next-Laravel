eb12ea03adbcdce801acdc7fac10832ff6ef0729
ugolnikov <hacker.2282005@mail.ru>
2025-01-12 07:44:40 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['via.placeholder.com', 'localhost'], 
+    domains: ['via.placeholder.com', 'localhost', 'next-js-client-for-next-laravel-production.up.railway.app', 'api-deploy-production-a967.up.railway.app'], 
   },
 }
 
